21fc27e07ca8fb31c910107c1bac8bdcc58d6886
Mitali <miitali.gupta@gmail.com>
2022-03-12 11:30:15 +0530

    trying to connect contact page using php

---
+++ php/contact.php
@@ -0,0 +1,19 @@
+<?php
+$name=$_POST["name"];
+$email=$_POST["email"];
+$sub=$_POST["subject"];
+$msg=$_POST["msg"];
+$conn = new mysqli("altruists.ctpunwarlucf.us-east-1.rds.amazonaws.com","admin","Loafer123","altruists",3306);
+$sql = "INSERT INTO CONTACT(Names,Email,Message,Subjects) VALUES ('$name','$email','$msg','$sub')";
+
+if ($conn->query($sql) === TRUE) 
+{
+  echo "New record created successfully<BR><BR><BR>";
+} 
+else 
+{
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
