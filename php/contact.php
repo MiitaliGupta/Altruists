1c559f8d75e669461c98a4bdfc7a1c7ab96376d7
Mitali <miitali.gupta@gmail.com>
2022-03-12 13:23:20 +0530

    contact page connected

--- php/contact.php
+++ php/contact.php
@@ -3,8 +3,8 @@ $name=$_POST["name"];
 $email=$_POST["email"];
 $sub=$_POST["subject"];
 $msg=$_POST["msg"];
-$conn = new mysqli("altruists.ctpunwarlucf.us-east-1.rds.amazonaws.com","admin","Loafer123","altruists",3306);
-$sql = "INSERT INTO CONTACT(Names,Email,Message,Subjects) VALUES ('$name','$email','$msg','$sub')";
+$conn = new mysqli("altruists.ctpunwarlucf.us-east-1.rds.amazonaws.com","admin","Loafer123","Altruists",3306);
+$sql = "INSERT INTO Contact(Names,Email,Message,Subjects) VALUES ('$name','$email','$msg','$sub')";
 
 if ($conn->query($sql) === TRUE) 
 {
