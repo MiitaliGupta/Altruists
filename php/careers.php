41080587c6b2cfc4b1098f7646f78cdb778322e9
Mitali Gupta <miitali.gupta@gmail.com>
2022-04-01 00:16:10 +0530

    -

--- php/careers.php
+++ php/careers.php
@@ -19,24 +19,27 @@ $img=$_POST["img"];
 $addid = "";
 $infoid = "";
 $row = "";
+$id = "";
 
 $conn = new mysqli("altruists.ctpunwarlucf.us-east-1.rds.amazonaws.com","admin","Loafer123","Altruists",3306);
 
 $sql1 = "INSERT INTO Address(Line1,Line2,City,State,PIN) VALUES ('$line1','$line2','$city','$state','$pin')";
 $sql2 = "SELECT MAX(Add_Id) FROM Address";
-$sql3 = "INSERT INTO PersonalInfo(Names,DOB,Email,ContactNo,AdhaarNo,Add_Id,Gender) VALUES ('$name','$dob','$email','$num','$adhaar','". $row['MAX(Add_Id)'] ."','$gender')";
+$sql3 = "INSERT INTO PersonalInfo(Names,DOB,Email,ContactNo,AdhaarNo,Add_Id,Gender) VALUES ('$name','$dob','$email','$num','$adhaar','$row','$gender')";
 $sql4 = "SELECT MAX(Info_Id) FROM PersonalInfo ";
-$sql5 = "INSERT INTO Volunteer(Info_Id,Image) VALUES ('".$row['MAX(Info_Id)']."','$img')";
+$sql5 = "INSERT INTO Volunteer(Info_Id,Image) VALUES ('$row','$img')";
 
 $conn->query($sql1);
 
 $addid = $conn->query($sql2) ;
 $row = mysqli_fetch_array($addid);
+// $id = $row['MAX(Add_Id)'];
 
 $conn->query($sql3);
 
 $infoid = $conn->query($sql4);    
-$row = mysqli_fetch_array($infoid);   
+$row = mysqli_fetch_array($infoid); 
+// $id = $row['MAX(Info_Id)'];
 
 if ($conn->query($sql5) === TRUE) 
 {
