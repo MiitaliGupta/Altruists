3c56c9c27c11c71afb66e4740d7c2cebfd541271
Mitali Gupta <miitali.gupta@gmail.com>
2022-04-04 21:32:04 +0530

    idr

--- php/careers.php
+++ php/careers.php
@@ -25,37 +25,37 @@ $conn = new mysqli("altruists.ctpunwarlucf.us-east-1.rds.amazonaws.com","admin",
 
 $sql1 = "INSERT INTO Address(Line1,Line2,City,State,PIN) VALUES ('$line1','$line2','$city','$state','$pin')";
 $sql2 = "SELECT MAX(Add_Id) FROM Address";
-$sql3 = "INSERT INTO PersonalInfo(Names,DOB,Email,ContactNo,AdhaarNo,Add_Id,Gender) VALUES ('$name','$dob','$email','$num','$adhaar','$row','$gender')";
+$sql3 = "INSERT INTO PersonalInfo(Names,DOB,Email,ContactNo,AdhaarNo,Add_Id,Gender) VALUES ('$name','$dob','$email','$num','$adhaar','$id','$gender')";
 $sql4 = "SELECT MAX(Info_Id) FROM PersonalInfo ";
-$sql5 = "INSERT INTO Volunteer(Info_Id,Image) VALUES ('$row','$img')";
+$sql5 = "INSERT INTO Volunteer(Info_Id,Image) VALUES ('$id','$img')";
 
 $conn->query($sql1);
 
 $addid = $conn->query($sql2);
-$row = mysqli_fetch_array($addid);
-// $id = $row['MAX(Add_Id)'];
-echo $row;
+$row =mysqli_fetch_array($addid);
+$id = $row['MAX(Add_Id)'];
+// echo $id;
 
-// $conn->query($sql3);
+$conn->query($sql3);
 
-// $infoid = $conn->query($sql4);    
-// $row = mysqli_fetch_array($infoid); 
-// // $id = $row['MAX(Info_Id)'];
+$infoid = $conn->query($sql4);    
+$row = mysqli_fetch_array($infoid); 
+$id = $row['MAX(Info_Id)'];
 
-// if ($conn->query($sql5) === TRUE) 
-// {
-// //echo "New record created successfully<BR><BR><BR>";
-// echo "<script LANGUAGE='JavaScript'>
-// window.alert('Successfully Registered as Volunteer');
-// window.location. href='http://localhost:3307/Altruists/careers.html';
-// </script>";
-// } 
-// else 
-// {
-//   // echo "Error: " . $sql . "<br>" . $conn->error;
-//   echo "<script LANGUAGE='JavaScript'>
-// window.alert('Volunteer Registration Unuccessfully');
-// window.location. href='http://localhost:3307/Altruists/careers.html';
-// </script>";
-// }
+if ($conn->query($sql5) === TRUE) 
+{
+//echo "New record created successfully<BR><BR><BR>";
+echo "<script LANGUAGE='JavaScript'>
+window.alert('Successfully Registered as Volunteer');
+window.location. href='http://localhost:3307/Altruists/careers.html';
+</script>";
+} 
+else 
+{
+  // echo "Error: " . $sql . "<br>" . $conn->error;
+  echo "<script LANGUAGE='JavaScript'>
+window.alert('Volunteer Registration Unuccessfully');
+window.location. href='http://localhost:3307/Altruists/careers.html';
+</script>";
+}
 $conn->close();
