5437182d08312a08bd9f50f7cb896373e4bb042f
Mitali Gupta <miitali.gupta@gmail.com>
2022-04-08 19:13:24 +0530

    ---

--- php/contact.php
+++ php/contact.php
@@ -4,25 +4,26 @@ $email=$_POST["email"];
 $sub=$_POST["subject"];
 $msg=$_POST["msg"];
 $conn = new mysqli("altruists.ctpunwarlucf.us-east-1.rds.amazonaws.com","admin","Loafer123","Altruists",3306);
-$sql = "INSERT INTO Contact(Names,Email,Message,Subjects) VALUES ('$name','$email','$msg','$sub')";
+$sql = "INSERT INTO Contact(Names,Email,Message,Subject) VALUES ('$name','$email','$msg','$sub')";
 
 //$conn->query($sql);
 if ($conn->query($sql) === TRUE) 
 {
 //echo "New record created successfully<BR><BR><BR>";
-echo "<script LANGUAGE='JavaScript'>
-window.alert('Form Submitted Successfully');
-window.location. href='http://localhost:3307/Altruists/contact.html';
-</script>";
+echo '<script LANGUAGE="JavaScript">
+alert("Form Submitted Successfully");
+window.location. href="http://localhost:3307/Altruists/contact.html";
+</script>';
 
 } 
 else 
 {
   // echo "Error: " . $sql . "<br>" . $conn->error;
-  echo "<script LANGUAGE='JavaScript'>
-window.alert('Form Submition Unuccessfully');
-window.location. href='http://localhost:3307/Altruists/contact.html';
-</script>";
+  '<script LANGUAGE="JavaScript">
+alert("Form Submition Unsuccessful");
+window.location. href="http://localhost:3307/Altruists/contact.html";
+</script>';
+
 }
 $conn->close();
 ?>
