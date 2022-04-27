6880c99d7cb7d8de79b4c993789ad61dff26d02f
Mitali Gupta <miitali.gupta@gmail.com>
2022-04-27 15:21:21 +0530

    ..

--- php/login.php
+++ php/login.php
@@ -13,19 +13,21 @@ $con = new mysqli("altruists.ctpunwarlucf.us-east-1.rds.amazonaws.com","admin","
          
         if($count == 1)
         {  
-            // include 'admin.html';
-            echo '<script LANGUAGE="JavaScript">
-window.location. href="http://localhost:3307/Altruists/admin.html";
-</script>'; 
+            if($user === "admin")
+            {
+                echo '<script LANGUAGE="JavaScript">
+                window.location. href="http://localhost:3307/Altruists/admin.html";
+                </script>';   
+            }
         }  
         else
         {  
             // echo "<center>Invalid Username or Password</center>";
             // echo "<center><button type='button' name='sign' class='button2' onclick='history.back()'>Back</button></center>";
             echo'<script LANGUAGE="JavaScript">
-alert("INVALID USERID or PASSWORD");
-window.location. href="http://localhost:3307/Altruists/login.html";
-</script>';
+            alert("INVALID USERID or PASSWORD");
+            window.location. href="http://localhost:3307/Altruists/login.html";
+            </script>';
              
         }   
         
